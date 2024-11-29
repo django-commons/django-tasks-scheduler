e6b9923460091dc0a2e5aeca8f11e5fb57952628
Daniel M <daniel.maruani@gmail.com>
2024-11-29 15:19:41 -0500

    rename

--- scheduler/models/__init__.py
+++ scheduler/models/__init__.py
@@ -1,5 +1,5 @@
 from .args import TaskKwarg, TaskArg
-from .scheduled_task import BaseTask, ScheduledTask, RepeatableTask, CronTask
+from .old_scheduled_task import BaseTask, ScheduledTask, RepeatableTask, CronTask
 from .queue import Queue
 from .task import Task
 
