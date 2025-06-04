070fab704ceaa7aca0861bf0d4c0d627e1722045
Daniel M <daniel.maruani@gmail.com>
2025-06-04 10:15:35 -0400

    fix:SCHEDULER_CONFIG as dict #273

--- scheduler/tests/test_settings.py
+++ scheduler/tests/test_settings.py
@@ -11,19 +11,20 @@ class TestWorkerAdmin(SchedulerBaseCase):
         self.assertEqual(SCHEDULER_CONFIG.EXECUTIONS_IN_PAGE, 20)
         settings.SCHEDULER_CONFIG = dict(
             EXECUTIONS_IN_PAGE=1,
-            SCHEDULER_INTERVAL=10,
+            SCHEDULER_INTERVAL=60,
             BROKER=Broker.REDIS,
-            CALLBACK_TIMEOUT=60,  # Callback timeout in seconds (success/failure/stopped)
-            # Default values, can be overriden per task/job
-            DEFAULT_SUCCESS_TTL=10 * 60,  # Time To Live (TTL) in seconds to keep successful job results
-            DEFAULT_FAILURE_TTL=365 * 24 * 60 * 60,  # Time To Live (TTL) in seconds to keep job failure information
-            DEFAULT_JOB_TTL=10 * 60,  # Time To Live (TTL) in seconds to keep job information
-            DEFAULT_JOB_TIMEOUT=5 * 60,  # timeout (seconds) for a job
+            CALLBACK_TIMEOUT=1111,
+
+            DEFAULT_SUCCESS_TTL=1111,
+            DEFAULT_FAILURE_TTL=111111,
+            DEFAULT_JOB_TTL=1111,
+            DEFAULT_JOB_TIMEOUT=11111,
             # General configuration values
-            DEFAULT_WORKER_TTL=10 * 60,  # Time To Live (TTL) in seconds to keep worker information after last heartbeat
-            DEFAULT_MAINTENANCE_TASK_INTERVAL=10 * 60,  # The interval to run maintenance tasks in seconds. 10 minutes.
-            DEFAULT_JOB_MONITORING_INTERVAL=30,  # The interval to monitor jobs in seconds.
-            SCHEDULER_FALLBACK_PERIOD_SECS=120,  # Period (secs) to wait before requiring to reacquire locks
+            DEFAULT_WORKER_TTL=11111,
+            DEFAULT_MAINTENANCE_TASK_INTERVAL=111,
+            DEFAULT_JOB_MONITORING_INTERVAL=1111,
+            SCHEDULER_FALLBACK_PERIOD_SECS=1111,
         )
         conf_settings()
-        self.assertEqual(SCHEDULER_CONFIG.EXECUTIONS_IN_PAGE, 1)
+        for key, value in settings.SCHEDULER_CONFIG.items():
+            self.assertEqual(getattr(SCHEDULER_CONFIG, key), value)
