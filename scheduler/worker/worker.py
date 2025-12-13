01a9d2247fb3db05b1b1e6793269c5851e46d8a4
Gabriele-V <gabriele-v@users.noreply.github.com>
2025-12-13 19:06:15 +0100

    Fix syntax error in worker.py

--- scheduler/worker/worker.py
+++ scheduler/worker/worker.py
@@ -104,7 +104,7 @@ class Worker:
         maintenance_interval: int = SCHEDULER_CONFIG.DEFAULT_MAINTENANCE_TASK_INTERVAL,
         job_monitoring_interval: int = SCHEDULER_CONFIG.DEFAULT_JOB_MONITORING_INTERVAL,
         dequeue_strategy: DequeueStrategy = DequeueStrategy.DEFAULT,
-        fork_job_execution: bool = platform.system() != 'Windows',,
+        fork_job_execution: bool = platform.system() != "Windows",
         with_scheduler: bool = True,
         burst: bool = False,
         model: Optional[WorkerModel] = None,
