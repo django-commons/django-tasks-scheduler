582148d9b4658e269042e2a64a10c251846bbf89
Dhaval Gojiya <dhavalgojiya10@gmail.com>
2026-09-10 18:40:29 +0530

    chore: modernize code for the minimum supported django

--- testproject/testproject/wsgi.py
+++ testproject/testproject/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for testproject project.
+WSGI config for testproject.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
